55d4531d27443f5602e0c1c65429f61296e059c9
Vitalii <Anoshkin.home@gmail.com>
2022-06-10 13:00:13 +0300

    вернулся на исходную, открыл доступ в интернет.

--- app/src/main/java/com/weatherforecast/MainActivity.java
+++ app/src/main/java/com/weatherforecast/MainActivity.java
@@ -1,13 +1,23 @@
 package com.weatherforecast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.TextView;
+
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvForecast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvForecast = findViewById(R.id.tvForecast);
+
+        //Retrofit retrofit = new Retrofit.Builder().baseUrl("")
     }
 }
